a249bd7fb9f2455477d4ff255f7ddd7960a15724
HHyeon <sunki1123@naver.com>
2021-03-31 18:34:20 +0900

    some actions for handling image

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -36,6 +36,7 @@
 #include <esp_http_server.h>
 #include <http_parser.h>
 
+#include "esp_spiffs.h"
 
 
 const char *TAG = "mainsession";
@@ -55,7 +56,6 @@ static void tcp_client_task(void *arg)
 
 		if(evtbit>>0)
 		{
-			xEventGroupClearBits(wifi_connection_groupevt, BIT(0));
 			break;
 		}
 	
@@ -91,7 +91,7 @@ static void tcp_client_task(void *arg)
 		}
 
 		rx_buffer[len] = 0;
-		ESP_LOGI(TAG, "recv - %s", rx_buffer);
+		//ESP_LOGI(TAG, "recv - %s", rx_buffer);
 		
 		static const char *payload = "Message from ESP32 ";
 		
@@ -118,36 +118,189 @@ quit:
 	
 }
 
-EventGroupHandle_t gpiomonevt;
 
-static void gpiowrtiteout_task(void *arg)
+void tcp_server_task(void *arg)
 {
+    char rx_buffer[128];
+    char addr_str[128];
+	
+	struct sockaddr_in destAddr;
+	destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	destAddr.sin_family = AF_INET;
+	destAddr.sin_port = htons(1234);
+	inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
+
+	int listen_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (listen_sock < 0) {
+		ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(TAG, "Socket created");
+
+	int err = bind(listen_sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
+	if (err != 0) {
+		ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(TAG, "Socket binded");
+	
+	err = listen(listen_sock, 3);
+	if (err != 0) {
+		ESP_LOGE(TAG, "Error occured during listen: errno %d", errno);
+		return;
+	}
+	ESP_LOGI(TAG, "Socket listening");
+	
 	while(1)
 	{
-		EventBits_t evtbit = xEventGroupGetBits(gpiomonevt);
+		struct sockaddr_in sourceAddr;
+		uint addrLen = sizeof(sourceAddr);
+		int sock = accept(listen_sock, (struct sockaddr *)&sourceAddr, &addrLen);
+		if (sock < 0) {
+			ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+			continue;
+		}
+		inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+		ESP_LOGI(TAG, "Socket accepted from %s", addr_str);
 		
-		if((evtbit>>GPIO_NUM_4) & 1)
-			gpio_set_level(GPIO_NUM_5, 1);
-		else
-			gpio_set_level(GPIO_NUM_5, 0);
+		
+		
 			
-	
-		vTaskDelay(50 / portTICK_PERIOD_MS);
+		ESP_LOGI(TAG, "Initializing SPIFFS");
+
+		esp_vfs_spiffs_conf_t conf = {
+		  .base_path = "/spiffs",
+		  .partition_label = NULL,
+		  .max_files = 5,
+		  .format_if_mount_failed = true
+		};
+
+		// Use settings defined above to initialize and mount SPIFFS filesystem.
+		// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+		esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+		if (ret != ESP_OK) {
+			if (ret == ESP_FAIL) {
+				ESP_LOGE(TAG, "Failed to mount or format filesystem");
+			} else if (ret == ESP_ERR_NOT_FOUND) {
+				ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+			} else {
+				ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+			}
+			return;
+		}
+		
+		size_t total = 0, used = 0;
+		ret = esp_spiffs_info(NULL, &total, &used);
+		if (ret != ESP_OK) {
+			ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+		} else {
+			ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+		}
+		
+		
+		
+		/*
+		ESP_LOGI(TAG, "Opening file");
+		FILE* file = fopen("/spiffs/img.jpg", "wb");
+		if (file == NULL) {
+			ESP_LOGE(TAG, "Failed to open file for writing");
+			return;
+		}
+		int totalreceivedbytes = 0;
+		int rlen = 1;
+		while( rlen > 0)
+		{
+			rlen = recv(sock, rx_buffer, sizeof(rx_buffer), 0 );
+			fwrite(rx_buffer, 1, rlen, file);
+			totalreceivedbytes+=rlen;
+			//ESP_LOGI(TAG, "%d", totalreceivedbytes);
+			if(rlen!=sizeof(rx_buffer)) break;
+		}
+		fclose(file);		
+		ESP_LOGI(TAG, "totalreceivedbytes : %d", totalreceivedbytes);
+		*/
+		
+		FILE* file = fopen("/spiffs/img.jpg", "rb");
+		if (file == NULL) {
+			ESP_LOGE(TAG, "Failed to open file for reading");
+			return;
+		}
+		int totalsentbytes = 0;
+		int rlen = 1, wlen;
+		while(rlen)
+		{
+			rlen = fread(rx_buffer, 1, sizeof(rx_buffer), file);
+			wlen = send(sock, rx_buffer, rlen, 0);
+			totalsentbytes += wlen;
+		}
+		fclose(file);
+		ESP_LOGI(TAG, "sent : %d", totalsentbytes);
+
+			
+		esp_vfs_spiffs_unregister(NULL);
+		
+        if (sock != -1) {
+            shutdown(sock, 0);
+            close(sock);
+        }
 	}
+	
+	ESP_LOGI(TAG, "Socket server terminated");
+	
+    vTaskDelete(NULL);
 }
 
 
-static void gpioreadin_task(void *arg)
+void gpio_oneshot(void *arg)
 {
-	while(1)
-	{
-		if(gpio_get_level(GPIO_NUM_4))
-			xEventGroupSetBits(gpiomonevt, BIT(GPIO_NUM_4));
-		else
-			xEventGroupClearBits(gpiomonevt, BIT(GPIO_NUM_4));
-		
-		vTaskDelay(50 / portTICK_PERIOD_MS);
+	gpio_set_level(GPIO_NUM_5, 1);
+	vTaskDelay(500 / portTICK_PERIOD_MS);
+	gpio_set_level(GPIO_NUM_5, 0);
+
+	vTaskDelete(NULL);
+}
+
+
+
+void spiffs_demo_task(void *arg)
+{
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+
+	esp_vfs_spiffs_conf_t conf = {
+	  .base_path = "/spiffs",
+	  .partition_label = NULL,
+	  .max_files = 5,
+	  .format_if_mount_failed = true
+	};
+
+	// Use settings defined above to initialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret == ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+		}
+		return;
 	}
+	
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(NULL, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+	}
+	
+
+	esp_vfs_spiffs_unregister(NULL);
+
+	vTaskDelete(NULL);
 }
 
 esp_err_t hello_type_get_handler(httpd_req_t *req)
@@ -158,6 +311,23 @@ esp_err_t hello_type_get_handler(httpd_req_t *req)
 	buf[qrylen] = 0;
 	
 	ESP_LOGI(TAG, "querystr - %d:%s", qrylen, buf);
+
+	if(strcmp(buf, "on") == 0)
+	{
+		gpio_set_level(GPIO_NUM_5, 1);
+	}
+	else if(strcmp(buf, "off") == 0)
+	{
+		gpio_set_level(GPIO_NUM_5, 0);
+	}
+	else if(strcmp(buf, "shot") == 0)
+	{
+		xTaskCreate(gpio_oneshot, "gpio_oneshot", 1024, NULL, 5, NULL);
+	}
+	else if(strcmp(buf, "spiffs") == 0)
+	{
+		xTaskCreate(spiffs_demo_task, "tcp_client", 2048, NULL, 5, NULL);
+	}
 	
     free (buf);
 	
@@ -168,11 +338,13 @@ esp_err_t hello_type_get_handler(httpd_req_t *req)
 #undef STR
 }
 
+
+
 httpd_uri_t basic_handlers = {
-		.uri = "/test",
-		.method = HTTP_GET,
-		.handler = hello_type_get_handler,
-		.user_ctx = NULL
+	.uri = "/test",
+	.method = HTTP_GET,
+	.handler = hello_type_get_handler,
+	.user_ctx = NULL
 };
 
 
@@ -228,10 +400,9 @@ void wifi_event_got_ip(httpd_handle_t* server)
 {
 	gpio_set_level(GPIO_NUM_2, 1);
 	
+	xEventGroupClearBits(wifi_connection_groupevt, BIT(0));
+    xTaskCreate(tcp_client_task, "tcp_client", 2048, NULL, 5, NULL);
 	
-	/*xEventGroupClearBits(wifi_connection_groupevt, BIT(0));
-    xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);*/
-
     if (*server == NULL) {
         ESP_LOGI(TAG, "Starting webserver");
         *server = start_httpd();
@@ -243,7 +414,7 @@ void wifi_event_disconnected(httpd_handle_t* server)
 {
 	gpio_set_level(GPIO_NUM_2, 0);
 	
-	/*xEventGroupSetBits(wifi_connection_groupevt, BIT(0));*/
+	xEventGroupSetBits(wifi_connection_groupevt, BIT(0));
 	
     if (*server) {
         ESP_LOGI(TAG, "Stopping webserver");
@@ -254,12 +425,9 @@ void wifi_event_disconnected(httpd_handle_t* server)
 
 void app_main()
 {
-	wifi_connection_groupevt = xEventGroupCreate();
-	gpiomonevt = xEventGroupCreate();
-
 	ESP_LOGI(TAG, "ESP8266 app_main Started");
-	xTaskCreate(gpiowrtiteout_task, "gpiowrtiteout task", 1024, NULL, 10, NULL);
-	xTaskCreate(gpioreadin_task, "gpioreadin_task", 1024, NULL, 10, NULL);
+	
+	wifi_connection_groupevt = xEventGroupCreate();
 	
 	gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
 	gpio_set_intr_type(GPIO_NUM_5, GPIO_INTR_DISABLE);
@@ -275,11 +443,15 @@ void app_main()
 	io_conf.pin_bit_mask = BIT(GPIO_NUM_4);
 	io_conf.pull_down_en = 1;
 	gpio_config(&io_conf);
+	
+	
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 	example_set_connection_info("ipTIMEA2003NS", "hyeon__1123");
 	ESP_ERROR_CHECK(example_connect());
+	
+    xTaskCreate(tcp_server_task, "tcp_server", 2048, NULL, 5, NULL);
 	
 }
